550e23ebcf033514be9b26db34b837078c71423d
Emily Dowdle <emily@editingemily.com>
2015-11-09 12:37:04 -0700

    Initial integration tests: - test site visitor can view stickers (on index page) - test admin can add sticker - test admin can login

---
+++ test/integration/visitor_can_view_items_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class VisitorView < ActionDispatch::IntegrationTest
+
+  test 'site visitor can view stickers' do
+    sticker = Sticker.create(title: "Awesome sticker",
+                             image: "some-url",
+                             price: 2)
+
+    visit stickers_path
+
+    assert page.has_content?("Awesome sticker")
+    assert page.has_content?(2)
+  end
+
+
+
+end
