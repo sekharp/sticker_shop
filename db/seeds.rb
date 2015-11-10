2197e56cc51ede417afb2f4bbbcd73b94b26eed3
Jason Pilz <jasonpilz@gmail.com>
2015-11-10 09:20:22 -0700

    Adjust seeds.rb
    - Add categories
    - Nest stickers in categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,28 @@ class Seed
   end
 
   def generate
+    create_categories
     create_stickers
   end
 
+  def create_categories
+    @javascript_category = Category.create(title: "javascript")
+    @ruby_category = Category.create(title: "ruby")
+    @browser_category = Category.create(title: "browser")
+  end
+
   def create_stickers
-    Sticker.create(title: "Node.js", image_url: "http://devstickers.com/assets/img/cat/nodejs.png", price: 6)
-    Sticker.create(title: "React.js", image_url: "http://devstickers.com/assets/img/cat/react-js.png", price: 8)
-    Sticker.create(title: "Ruby", image_url: "http://devstickers.com/assets/img/cat/ruby.png", price: 82)
-    Sticker.create(title: "Chrome", image_url: "http://devstickers.com/assets/img/cat/chrome.png", price: 2)
+    @javascript_category.stickers.create(title: "Node.js",
+                                         image_url: "http://devstickers.com/assets/img/cat/nodejs.png", price: 6)
+    @javascript_category.stickers.create(title: "React.js",
+                                         image_url: "http://devstickers.com/assets/img/cat/react-js.png", price: 8)
+    @ruby_category.stickers.create(title: "Ruby",
+                                   image_url: "http://devstickers.com/assets/img/cat/ruby.png", price: 82)
+    @browser_category.stickers.create(title: "Chrome",
+                                      image_url: "http://devstickers.com/assets/img/cat/chrome.png", price: 2)
     puts "#{Sticker.all.map(&:title).join(", ")} created."
   end
+
 end
 
 Seed.start
