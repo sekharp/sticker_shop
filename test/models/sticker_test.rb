ff01e4084dfd549d68bc429975c34597acf98f62
Jason Pilz <jasonpilz@gmail.com>
2015-11-09 18:02:53 -0700

    Update sticker_test.rb

--- test/models/sticker_test.rb
+++ test/models/sticker_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class StickerTest < ActiveSupport::TestCase
   test "sticker is valid" do
-    sticker = Sticker.new(title: "Node.js", image_url: "http://devstickers.com/assets/img/cat/nodejs.png", price: 6)
+    sticker = Sticker.new(title: "Node.js",
+                          image_url: "http://devstickers.com/assets/img/cat/nodejs.png",
+                          price: 6)
 
     assert sticker.valid?
   end
