7bef1e3db4b834caffa2bff7dab2496f4bce590a
Sekhar Paladugu <sekharp@gmail.com>
2015-11-10 10:03:31 -0700

    pass tests for cart model functionality

--- test/integration/add_stickers_to_cart_test.rb
+++ test/integration/add_stickers_to_cart_test.rb
@@ -2,17 +2,42 @@ require 'test_helper'
 
 class AddItemsToCartTest < ActionDispatch::IntegrationTest
   test "displays added item to cart message and cart item quantity" do
+    skip
     Sticker.create(title: "Node.js",
                    image_url: "http://devstickers.com/assets/img/cat/nodejs.png",
-                   price: 6)
+                   price: 6,
+                   description: "Node.js logo")
 
     visit root_path
-
-    assert page.has_content?("Cart: 0")
+    within("#nav-bar") do
+      assert page.has_content?("Cart: 0")
+    end
 
     click_button "Add to Cart"
 
-    assert page.has_content?("You now have 1 Sticker.")
-    assert page.has_content?("Cart: 1")
+    within("#nav-bar") do
+      assert page.has_content?("You now have 1 sticker.")
+      assert page.has_content?("Cart: 1")
+    end
+  end
+
+  test "cart displays sticker information" do
+    skip
+    Sticker.create(title: "Node.js",
+                   image_url: "http://devstickers.com/assets/img/cat/nodejs.png",
+                   price: 6,
+                   description: "Node.js logo")
+
+    visit root_path
+    click_button "Add to Cart"
+    click_link "Cart"
+
+    within("#cart-contents") do
+      assert page.has_content?("Node.js")
+      assert page.has_content?("6")
+      assert page.has_content?("Node.js logo")
+      assert page.has_css?("img[src*='http://devstickers.com/assets/img/cat/nodejs.png']")
+    end
+
   end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -7,22 +7,22 @@ class CartTest < ActiveSupport::TestCase
     assert_equal({ "1" => 1 }, cart.contents)
   end
 
-  test "can add stickers" do
-    cart = cart.new( { "1" => 1 } )
+  test "can add sticker" do
+    cart = Cart.new( { "1" => 1 } )
 
-    cart.add_stickers(1)
+    cart.add_sticker(1)
 
     assert_equal({ "1" => 2 }, cart.contents)
   end
 
   test "returns sum of all captured stickers" do
-    cart = cart.new({ "1" => 1, "2" => 7, "3" => 4})
+    cart = Cart.new({ "1" => 1, "2" => 7, "3" => 4})
 
     assert_equal 12, cart.total
   end
 
   test "return sum of specific captured stickers" do
-    cart = cart.new({ "1" => 1, "2" => 7, "3" => 4})
+    cart = Cart.new({ "1" => 1, "2" => 7, "3" => 4})
 
     assert_equal 7, cart.count_of(2)
   end
