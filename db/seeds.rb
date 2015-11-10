0548ec8c573401312c0a2e5314667590e51f9254
Jason Pilz <jasonpilz@gmail.com>
2015-11-10 09:31:17 -0700

    Add description column to sticker model

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,21 @@ class Seed
 
   def create_stickers
     @javascript_category.stickers.create(title: "Node.js",
-                                         image_url: "http://devstickers.com/assets/img/cat/nodejs.png", price: 6)
+                                         image_url: "http://devstickers.com/assets/img/cat/nodejs.png",
+                                         price: 6,
+                                         description: "Node.js logo")
     @javascript_category.stickers.create(title: "React.js",
-                                         image_url: "http://devstickers.com/assets/img/cat/react-js.png", price: 8)
+                                         image_url: "http://devstickers.com/assets/img/cat/react-js.png",
+                                         price: 8,
+                                         description: "React.js logo")
     @ruby_category.stickers.create(title: "Ruby",
-                                   image_url: "http://devstickers.com/assets/img/cat/ruby.png", price: 82)
+                                   image_url: "http://devstickers.com/assets/img/cat/ruby.png",
+                                   price: 82,
+                                   description: "Ruby logo")
     @browser_category.stickers.create(title: "Chrome",
-                                      image_url: "http://devstickers.com/assets/img/cat/chrome.png", price: 2)
+                                      image_url: "http://devstickers.com/assets/img/cat/chrome.png",
+                                      price: 2,
+                                      description: "Chrome logo")                                )
     puts "#{Sticker.all.map(&:title).join(", ")} created."
   end
 
