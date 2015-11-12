9a2af26f104277c00e8e14d835d79eb4eb48a211
Jason Pilz <jasonpilz@gmail.com>
2015-11-12 07:59:56 -0700

    First test for viewing past orders passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :orders
 
   validates :username, presence: true,
                        uniqueness: true
 
-  validates :password, presence: true                     
+  validates :password, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :show], param: :slug
   resources :cart_stickers, only: [:create, :index, :destroy]
   resources :cart, only: [:index, :create]
+  resources :users, only: [:new, :create]
+  resources :orders, only: [:create, :index]
   get '/login', to: "session#new"
   post '/login', to: "session#create"
   delete '/logout', to: "session#destroy"
-  resources :users, only: [:new, :create]
   get '/dashboard', to: "users#show"
 end
