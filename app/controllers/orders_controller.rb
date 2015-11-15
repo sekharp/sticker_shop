e65b45837b7dfa9705a7e0593b5c1ff1df250ea7
Sekhar Paladugu <sekharp@gmail.com>
2015-11-15 16:06:10 -0700

    finish functionality of order index page showing retired items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,8 @@ class OrdersController < ApplicationController
     if session[:user_id].nil?
       redirect_to '/login'
     else
-      @order = Order.create(status: "Ordered", user_id: current_user.id)
-      @order_stickers = session[:cart].map { |id, q| OrderSticker.create(quantity: q, sticker_id: id, order_id: @order.id) }
+      order = Order.create(status: "Ordered", user_id: current_user.id)
+      session[:cart].map { |id, q| OrderSticker.create(quantity: q, sticker_id: id, order_id: order.id) }
       session[:cart] = {}
       flash[:success] = "Order was successfully placed."
       redirect_to orders_path
