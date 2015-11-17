dd45fb21dc30a26981b765e23ab726121fcef671
Sekhar Paladugu <sekharp@gmail.com>
2015-11-17 13:54:02 -0700

    stash changes but branch tests still broken

--- app/controllers/cart_stickers_controller.rb
+++ app/controllers/cart_stickers_controller.rb
@@ -3,15 +3,31 @@ class CartStickersController < ApplicationController
     sticker = Sticker.find(params[:sticker_id])
     @cart.add_sticker(sticker.id)
     session[:cart] = @cart.contents
-    flash[:notice] = "Sticker added to cart"
+    flash[:success] = "Successfully added #{view_context.link_to "#{sticker.title}", sticker_path(sticker.id)} sticker to your cart.".html_safe
     redirect_to root_path
+  end
+
+  def update
+    sticker = Sticker.find(params[:sticker_id])
+    @cart.add_sticker(sticker.id)
+    session[:cart] = @cart.contents
+    flash[:success] = "Successfully added #{view_context.link_to "#{sticker.title}", sticker_path(sticker.id)} sticker to your cart.".html_safe
+    redirect_to cart_index_path
+  end
+
+  def edit
+    sticker = Sticker.find(params[:id])
+    @cart.subtract_sticker(sticker.id)
+    session[:cart] = @cart.contents
+    flash[:success] = "Successfully removed #{view_context.link_to "#{sticker.title}", sticker_path(sticker.id)} sticker from your cart.".html_safe
+    redirect_to cart_index_path
   end
 
   def destroy
     sticker = Sticker.find(params[:id])
-    @cart.subtract_sticker(sticker.id)
+    @cart.remove_sticker(sticker.id)
     session[:cart] = @cart.contents
-    flash[:notice] = "Sticker removed from cart"
+    flash[:success] = "Successfully removed #{view_context.link_to "#{sticker.title}", sticker_path(sticker.id)} sticker from your cart.".html_safe
     redirect_to cart_index_path
   end
 end
