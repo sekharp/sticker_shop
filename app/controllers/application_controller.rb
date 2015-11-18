9762f320bd09a843eb1ad2db44e1ef69d3922a60
Sekhar Paladugu <sekharp@gmail.com>
2015-11-18 14:27:33 -0700

    fix admin? error with incorrect login credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_cart
-  helper_method :current_user
+  helper_method :current_user, :current_admin?
 
   def set_cart
     @cart = Cart.new(session[:cart])
