bc841fcd6afc04ca1b35adf9263b8193fe5e2011
Emily Dowdle <emily@editingemily.com>
2015-11-18 11:38:03 -0700

    Test admin edit capability

--- test/integration/admin_edit_test.rb
+++ test/integration/admin_edit_test.rb
@@ -1,10 +1,3 @@
-# Background: an existing item
-#       As an admin
-#       When I visit "admin/items"
-#       And I click "Edit"
-#       Then my current path should be "/admin/items/:ITEM_ID/edit"
-#       And I should be able to upate title, description, image, and status
-
 require "test_helper"
 
 class AdminEditStickerTest < ActionDispatch::IntegrationTest
@@ -18,11 +11,41 @@ class AdminEditStickerTest < ActionDispatch::IntegrationTest
     fill_in "Username", with: "emily"
     fill_in "Password", with: "password"
     click_button "Login"
+
+    click_link "Add New Sticker"
+
+    assert new_admin_sticker_path, current_path
+
+    sticker = Sticker.create(title: "New Sticker",
+                             category: "Ruby",
+                             description: "New sticker description",
+                             price: 5)
   end
 
   test "admin can view edit path" do
-    
+    visit admin_stickers_path
 
+    click_button "Edit"
+
+    assert edit_admin_sticker_path(sticker.id), current_path
+
+    within("edit-sticker") do
+      assert page.has_content?("Title")
+      assert page.has_content?("New Sticker")
+      assert page.has_content?("Title")
+    end
+
+
+    #       And I click "Edit"
+    #       Then my current path should be "/admin/items/:ITEM_ID/edit"
+    #       And I should be able to upate title, description, image, and status
+
+    within("#nav-bar") do
+      assert page.has_content?("NEW STICKER sticker created")
+    end
+    within(".container") do
+      assert page.has_content?("New sticker description")
+    end
   end
 
 end
